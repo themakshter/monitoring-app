3b972b9998660e201297a9ef57d028827753dd20
themakshter <mohammad_ali93@live.com>
2020-04-27 23:37:49 +0100

    Add Toast and Beep for alarms
    This will show a Toast with message that a value is out of bounds and also beep to inform the user

--- components/AlarmMetricDisplay.js
+++ components/AlarmMetricDisplay.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import MetricDisplay from './MetricDisplay';
+import Toast from 'react-native-simple-toast';
+import RNBeep from 'react-native-a-beep';
 
 export default function AlarmMetricDisplay(props) {
 
@@ -7,6 +9,8 @@ export default function AlarmMetricDisplay(props) {
   
   function getStateFromValue(value){
     if(value < props.lowerLimit || value > props.upperLimit){
+      Toast.show('The patient rate has gone out of the allowed bounds!', Toast.LONG, Toast.TOP);
+      RNBeep.beep(false);
       return 'alarm';
     }
     return 'normal';
