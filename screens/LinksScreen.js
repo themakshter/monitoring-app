2329220f41cec5c85b2d10ce44e10051ba678328
themakshter <mohammad_ali93@live.com>
2020-04-29 01:35:44 +0100

    Move OptionButton into its own component
    This component was duplicated across multiple screens - it has now been consolidated into a separate component and imported into other pages

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,7 +1,7 @@
-import Icon from 'react-native-vector-icons/Ionicons';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RectButton, ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import OptionButton from '../components/OptionButton';
 
 export default function LinksScreen() {
   return (
@@ -13,23 +13,6 @@ export default function LinksScreen() {
   );
 }
 
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton
-      style={[styles.option, isLastOption && styles.lastOption]}
-      onPress={onPress}>
-      <View style={{ flexDirection: 'row' }}>
-        <View style={styles.optionIconContainer}>
-          <Icon name={icon} size={22} color="rgba(0,0,0,0.35)" />
-        </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
-        </View>
-      </View>
-    </RectButton>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -37,24 +20,5 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     paddingTop: 15,
-  },
-  optionIconContainer: {
-    marginRight: 12,
-  },
-  option: {
-    backgroundColor: '#fdfdfd',
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: '#ededed',
-  },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: 'flex-start',
-    marginTop: 1,
   },
 });
