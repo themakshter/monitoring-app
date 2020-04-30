4ab50375f40620392809006c6768f5af74551757
themakshter <mohammad_ali93@live.com>
2020-04-30 01:37:44 +0100

    Add dummy data generation
    This change adds a new component which will generate dummy data each second, calling the function it has been given with updated readings

---
+++ logic/DummyDataGenerator.js
@@ -0,0 +1,36 @@
+export default function dummyDataGenerator(updateReadingStateFunction) {
+  let intervalFunction;
+
+  function getRandomValue(range, valueToSubtract = 0) {
+    return Math.round(Math.random() * range - valueToSubtract);
+  }
+
+  function generateDummyReadings() {
+    return {
+      peep: getRandomValue(10),
+      peakPressure: getRandomValue(100, 100),
+      patientRate: getRandomValue(220),
+      vte: getRandomValue(700),
+      inspiratoryTime: getRandomValue(3),
+      expiratoryTime: getRandomValue(5).toFixed(1),
+      oxygen: getRandomValue(100),
+      flow: getRandomValue(30, 10),
+    };
+  }
+
+  function startGenerating() {
+    intervalFunction = setInterval(() => {
+      const newReadings = generateDummyReadings();
+      updateReadingStateFunction(newReadings);
+    });
+  }
+
+  function stopGenerating() {
+    clearInterval(intervalFunction);
+  }
+
+  return {
+    startGenerating,
+    stopGenerating,
+  };
+}
