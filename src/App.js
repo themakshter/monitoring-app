9c1066703a038f51818617437a856e8d40b561f2
themakshter <mohammad_ali93@live.com>
2020-04-30 03:09:22 +0100

    Remove unneeded components
    This change removes unneeded components from the main page to simplify it,
    such aas removing unused imports and some unnecessary logic.

--- src/App.js
+++ src/App.js
@@ -5,53 +5,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
-import useLinking from './navigation/useLinking';
 
 const Stack = createStackNavigator();
 
 export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-      }
-    }
-
-    loadResourcesAndDataAsync();
-  });
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer
-          ref={containerRef}
-          initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen
-              name="Main"
-              component={BottomTabNavigator}
-              initialParams={[34, 45]}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Main"
+            component={BottomTabNavigator}
+            initialParams={[34, 45]}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
