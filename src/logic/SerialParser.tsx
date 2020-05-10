a0fc5b536517cf11b32e8a665c14f18015d7ff04
Mohammad Ali Khan <mohammad_ali93@live.com>
2020-05-10 13:28:12 +0100

    Refactor SerialParser and update parse algorithm (#40)
    - The telemetry packet has changed so some changes were made to handle
      that. A copy of the latest packet definition has been included in the
      repository for reference.
    - Ventilation mode is an absolute value, present in 3 bits of the 29th
      byte. This change adds a new constant for ventilation modes and
      calculates the correct ventilation mode for the new packet.
    - There was a lot of repetition and bloat in the logic for updating the
      graphs for pressure, volume and flow rate. This logic has been
      consolidated into a single method to be re-used and hence improve
      readability.
    - Variables were renamed to be more understandable and in camelCase to
      follow JS standards.

--- src/logic/SerialParser.tsx
+++ src/logic/SerialParser.tsx
@@ -1,146 +1,100 @@
 import Constants from '../constants/Constants';
 import Alarms from '../constants/Alarms';
+import VentilationModes from '../constants/VentilationModes';
 
-let dummydata1 = new Array(0);
-let dummydata2 = new Array(0);
-let GraphPressure = new Array(Constants.GraphLength).fill(0);
-let GraphVolume = new Array(Constants.GraphLength).fill(0);
-let GraphFlow = new Array(Constants.GraphLength).fill(0);
-let TotalPacket = 0;
-let FailedPacket = 0;
-let Counter = 0;
-let Counter1 = 0;
-let Counter2 = 0;
+let pressureGraph = new Array(Constants.GraphLength).fill(0);
+let volumeGraph = new Array(Constants.GraphLength).fill(0);
+let flowRateGraph = new Array(Constants.GraphLength).fill(0);
+let totalPackets = 0;
+let failedPackets = 0;
+let interval = 0;
+let counterForGraphs = 0;
 
 export const processSerialData = (
-  Data: any,
+  packet: any,
   updateReadingStateFunction: (value: any) => void,
 ) => {
-  TotalPacket++;
-  if (processIntegrityCheck(Data)) {
-    dummydata1 = dummydata1.concat([
-      getWordFloat(Data[10], Data[11], 90 / 65535, -30),
-    ]);
-    dummydata2 = dummydata2.concat([
-      getWordFloat(Data[8], Data[9], 4000 / 65535, -2000),
-    ]);
-    // console.log(dummydata1.length);
-    if (dummydata1.length > Constants.UpdateInterval) {
-      // GraphPressure.splice(0, dummydata1.length);
-      // GraphPressure = GraphPressure.concat(dummydata1);
-      // GraphVolume.splice(0, dummydata2.length);
-      // GraphVolume = GraphVolume.concat(dummydata2);
-      // for (var i = 0; i < dummydata2.length; i++) {
-      //   GraphVolume[Counter++ % Constants.GraphLength] = dummydata2[i];
-      //   if (Counter >= Constants.GraphLength) {
-      //     Counter = 0;
-      //   }
-      // }
-      // for(var i = 0; i < 3; i++){
-      GraphVolume[Counter++ % Constants.GraphLength] =
-        dummydata2[dummydata2.length - 1];
-      if (Counter >= Constants.GraphLength) {
-        Counter = 0;
-      }
-      GraphVolume[Counter % Constants.GraphLength] = 0;
-      GraphVolume[(Counter + 1) % Constants.GraphLength] = 0;
-      GraphVolume[(Counter + 2) % Constants.GraphLength] = 0;
+  totalPackets++;
+  if (processIntegrityCheck(packet)) {
+    interval++;
+    if (interval > Constants.UpdateInterval) {
+      interval = 0;
 
-      GraphFlow[Counter2++ % Constants.GraphLength] = getWordFloat(
-        Data[12],
-        Data[13],
+      const measuredVolume = getWordFloat(
+        packet[8],
+        packet[9],
+        4000 / 65535,
+        -2000,
+      );
+      addValueToGraph(measuredVolume, volumeGraph, counterForGraphs);
+
+      const measuredFlowRate = getWordFloat(
+        packet[12],
+        packet[13],
         400 / 65535,
         -200,
       );
-      if (Counter2 >= Constants.GraphLength) {
-        Counter2 = 0;
-      }
-      GraphFlow[Counter2 % Constants.GraphLength] = 0;
-      GraphFlow[(Counter2 + 1) % Constants.GraphLength] = 0;
-      GraphFlow[(Counter2 + 2) % Constants.GraphLength] = 0;
+      addValueToGraph(measuredFlowRate, flowRateGraph, counterForGraphs);
 
-      // GraphVolume[(Counter + 3) % Constants.GraphLength] = 0;
-      // GraphVolume[(Counter + 4) % Constants.GraphLength] = 0;
+      const measuredPressure = getWordFloat(
+        packet[10],
+        packet[11],
+        90 / 65535,
+        -30,
+      );
+      addValueToGraph(measuredPressure, pressureGraph, counterForGraphs);
 
-      // for (var i = 0; i < dummydata1.length; i++) {
-      //   GraphPressure[Counter1++ % Constants.GraphLength] = dummydata1[i];
-      //   if (Counter1 >= Constants.GraphLength) {
-      //     Counter1 = 0;
-      //   }
-      // }
-      // for(var i = 0; i < 3; i++){
-
-      GraphPressure[Counter1++ % Constants.GraphLength] =
-        dummydata1[dummydata1.length - 1];
-      if (Counter1 >= Constants.GraphLength) {
-        Counter1 = 0;
+      counterForGraphs++;
+      if (counterForGraphs >= Constants.GraphLength) {
+        counterForGraphs = 0;
       }
 
-      GraphPressure[Counter1 % Constants.GraphLength] = 0;
-      GraphPressure[(Counter1 + 1) % Constants.GraphLength] = 0;
-      GraphPressure[(Counter1 + 2) % Constants.GraphLength] = 0;
-      // GraphPressure[(Counter1 + 3) % Constants.GraphLength] = 0;
-      // GraphPressure[(Counter1 + 4) % Constants.GraphLength] = 0;
-      //   if (Counter >= Constants.GraphLength){
-      //     Counter = 0;
-      //   }
-      // }
+      const ventilationMode = getVentilationMode(packet[29]);
 
-      //console.log(Data);
-      dummydata1 = [];
-      dummydata2 = [];
-      var ControlMode = '';
-      if ((Data[29] & 0x08) > 0) {
-        ControlMode = 'AC-';
-      }
-      if ((Data[29] & 0x04) > 0) {
-        ControlMode = ControlMode + 'PCV';
-      } else ControlMode = ControlMode + 'VCV';
+      let currentAlarms = getAlarmValues(packet);
 
-      let currentAlarms = getAlarmValues(Data);
-
-      // console.log('updateing');
       updateReadingStateFunction({
-        peep: Data[26] - 30,
-        measuredPressure: ((Data[10] + Data[11] * 256) * 90) / 65535 - 30,
-        plateauPressure: getWordFloat(Data[16], Data[17], 90 / 65535, -30),
-        patientRate: Data[23], //23
-        tidalVolume: getWordFloat(Data[20], Data[21], 1, 0),
-        ieRatio: (Data[24] & 0x0f) + ':' + (Data[24] & 0xf0) / 16,
-        vti: getWordFloat(Data[30], Data[31], 4000 / 65535, -2000),
-        vte: getWordFloat(Data[32], Data[33], 4000 / 65535, -2000),
-        minuteVentilation: getWordFloat(Data[34], Data[35], 40 / 65535, 0),
+        peep: packet[26] - 30,
+        measuredPressure: measuredPressure,
+        plateauPressure: getWordFloat(packet[16], packet[17], 90 / 65535, -30),
+        patientRate: packet[23],
+        tidalVolume: getWordFloat(packet[20], packet[21], 1, 0),
+        ieRatio: (packet[24] & 0x0f) + ':' + (packet[24] & 0xf0) / 16,
+        vti: getWordFloat(packet[30], packet[31], 4000 / 65535, -2000),
+        vte: getWordFloat(packet[32], packet[33], 4000 / 65535, -2000),
+        minuteVentilation: getWordFloat(packet[34], packet[35], 40 / 65535, 0),
         inspiratoryTime: 1,
         expiratoryTime: 5,
-        fiO2: Data[25], //25
-        flow: 23,
-        flowrate: 23,
-        PIP: Data[40] - 30,
-        mode: ControlMode,
-        graphPressure: GraphPressure,
-        graphVolume: GraphVolume,
-        graphFlow: GraphFlow,
+        fiO2: packet[25],
+        flowRate: measuredFlowRate,
+        PIP: packet[40] - 30,
+        mode: ventilationMode,
+        graphPressure: pressureGraph,
+        graphVolume: volumeGraph,
+        graphFlow: flowRateGraph,
+        packetIntegrityRatio: `${failedPackets} / ${totalPackets}`,
         alarms: currentAlarms,
       });
+      totalPackets++;
     }
   } else {
-    FailedPacket++;
+    failedPackets++;
   }
 };
 
-function processIntegrityCheck(Data: any): boolean {
-  if (Data.length > Constants.TotalPacketLength) {
+function processIntegrityCheck(packet: any): boolean {
+  if (packet.length > Constants.TotalPacketLength) {
     return false;
   }
   let crc = 0;
-  for (let i = 0; i < Data.length - 2; i++) {
-    crc = (crc ^ Data[i]) & 0xff;
+  for (let i = 0; i < packet.length - 2; i++) {
+    crc = (crc ^ packet[i]) & 0xff;
   }
-  if (crc == Data[Data.length - 2]) return true;
+  if (crc == packet[packet.length - 2]) return true;
   else {
     var data = '';
-    for (let i = 0; i < Data.length; i++) {
-      data = data + ' ' + parseInt(Data[i]);
+    for (let i = 0; i < packet.length; i++) {
+      data = data + ' ' + parseInt(packet[i]);
     }
     console.log('crc failed ' + data);
   }
@@ -154,6 +108,20 @@ function getWordFloat(
   offset: number,
 ): number {
   return (ByteL + ByteH * 256) * multiplier + offset;
+}
+
+function addValueToGraph(
+  value: number,
+  graph: number[],
+  counter: number,
+): void {
+  graph[counter++ % Constants.GraphLength] = value;
+  if (counter >= Constants.GraphLength) {
+    counter = 0;
+  }
+  graph[counter % Constants.GraphLength] = 0;
+  graph[(counter + 1) % Constants.GraphLength] = 0;
+  graph[(counter + 2) % Constants.GraphLength] = 0;
 }
 
 function getAlarmValues(serialData: Array<number>): Array<string> {
@@ -180,4 +148,12 @@ function getAlarmValues(serialData: Array<number>): Array<string> {
 function getValueOfBit(valueToParse: number, bitIndex: number) {
   const bitIndexNumberForFindingValue = [1, 2, 4, 8, 16, 32, 64, 128];
   return valueToParse & bitIndexNumberForFindingValue[bitIndex];
+}
+
+function getVentilationMode(valueToParse: number): string {
+  // 0x1C is 00011100 so we find the values contain in bits 2-4
+  // we also want the index to retrieve the correct mode from our array
+  // so we shift the bits to the end to get the actual value
+  const ventilationModeIndex = (valueToParse & 0x1c) >> 2;
+  return VentilationModes[ventilationModeIndex];
 }
