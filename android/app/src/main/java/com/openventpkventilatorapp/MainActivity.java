dc0719fdd8ff3ce4635202ce40cd77a78e33ca71
Mohammad Ali Khan <mohammad_ali93@live.com>
2020-07-10 00:13:27 +0100

    Update package namespaces for app (#121)
    This change updates the package names in the app to be more standardised, e.g. `com.openventpk.ventilatorapp` instead of `com.openventpkventilatorapp`

--- android/app/src/main/java/com/openventpkventilatorapp/MainActivity.java
+++ android/app/src/main/java/com/openventpkventilatorapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.openventpkventilatorapp;
+package com.openventpk.ventilatorapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "openventpkVentilatorApp";
+    return "OpenVentPkVentilatorApp";
   }
 }
--- android/app/src/main/java/com/openventpkventilatorapp/MainApplication.java
+++ android/app/src/main/java/com/openventpkventilatorapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.openventpkventilatorapp;
+package com.openventpk.ventilatorapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.openventpkventilatorapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.openventpk.ventilatorapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
